e746e0067f94f22ff846dccabd0dc60aca09017d
Bidon Pomoev <yoba123@yandex.ru>
2014-01-05 16:43:00 +0400

    new api worker implementation

--- src/com/archean/jtradeapi/ApiWorker.java
+++ src/com/archean/jtradeapi/ApiWorker.java
@@ -1,8 +1,11 @@
 package com.archean.jtradeapi;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
 
-public class ApiWorker implements Runnable {
+public class ApiWorker {
     public abstract static class Callback {
         public abstract void onUpdate(final ApiWorker.ApiDataType dataType, final Object data);
 
@@ -15,39 +18,103 @@ public class ApiWorker implements Runnable {
         ACCOUNT_BALANCES, ACCOUNT_ORDERS, ACCOUNT_HISTORY
     }
 
+    private class ApiWorkerTask implements Runnable {
+        ApiDataType apiDataType;
+        public ApiWorkerTask(ApiDataType dataType) {
+            this.apiDataType = dataType;
+        }
+        private Object retrieveData() throws Exception {
+            switch(apiDataType) {
+                case MARKET_PRICES:
+                    return tradeApi.getMarketPrices(pair);
+                case MARKET_DEPTH:
+                    return tradeApi.getMarketDepth(pair);
+                case MARKET_HISTORY:
+                    return tradeApi.getMarketHistory(pair);
+                case ACCOUNT_BALANCES:
+                    return tradeApi.getAccountBalances();
+                case ACCOUNT_ORDERS:
+                    return tradeApi.getAccountOpenOrders(pair);
+                case ACCOUNT_HISTORY:
+                    return tradeApi.getAccountHistory(pair);
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+        private void updateWorkerData(Object data) {
+            switch (apiDataType) {
+                case MARKET_PRICES:
+                    marketInfo.price = (BaseTradeApi.StandartObjects.Prices)data;
+                    break;
+                case MARKET_DEPTH:
+                    marketInfo.depth = (BaseTradeApi.StandartObjects.Depth)data;
+                    break;
+                case MARKET_HISTORY:
+                    marketInfo.history = (List<BaseTradeApi.StandartObjects.Order>)data;
+                    break;
+                case ACCOUNT_BALANCES:
+                    accountInfo.balance = (BaseTradeApi.StandartObjects.AccountInfo.AccountBalance)data;
+                    break;
+                case ACCOUNT_ORDERS:
+                    accountInfo.orders = (List<BaseTradeApi.StandartObjects.Order>)data;
+                    break;
+                case ACCOUNT_HISTORY:
+                    accountInfo.history = (List<BaseTradeApi.StandartObjects.Order>)data;
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+        @Override public void run() {
+            Object data;
+            while(!Thread.currentThread().isInterrupted()) {
+                try {
+                    data = retrieveData();
+                    updateWorkerData(data);
+                    if(callback != null) {
+                        callback.onUpdate(apiDataType, data);
+                    }
+                    Thread.sleep(timeInterval);
+                } catch(InterruptedException e) {
+                    break;
+                } catch(Exception e) {
+                    if(callback != null)
+                        callback.onError(e);
+                    else
+                        e.printStackTrace();
+                }
+            }
+        }
+    }
+
     volatile public BaseTradeApi.StandartObjects.AccountInfo accountInfo;
-    volatile public List<BaseTradeApi.StandartObjects.MarketInfo> marketInfo;
-    volatile private boolean updateAccountInfo = false;
-    volatile private boolean updateMarketInfo = false;
-    volatile private boolean retrieveMarketOrders = false;
-    volatile private boolean retrieveMarketHistory = false;
-    volatile private boolean retrieveAccountOrders = false;
-    volatile private boolean retrieveAccountHistory = false;
+    volatile public BaseTradeApi.StandartObjects.MarketInfo marketInfo;
     volatile public BaseTradeApi tradeApi = null;
-    volatile long timeInterval = 100;
-    volatile public boolean paused = false;
+    volatile long timeInterval = 200;
     volatile Object pair = null;
     volatile Callback callback = null;
+    volatile private Map<ApiDataType, Thread> threadMap = new TreeMap<>();
 
-    public void run() {
-        while (!Thread.currentThread().isInterrupted()) {
-            try {
-                if (!paused) {
-                    if (updateMarketInfo) {
-                        marketInfo = tradeApi.getMarketData(pair, retrieveMarketOrders, retrieveMarketHistory);
-                    }
-                    if (updateAccountInfo) {
-                        accountInfo = tradeApi.getAccountInfo(pair, retrieveAccountOrders, retrieveAccountHistory);
-                    }
-                    if (callback != null) callback.onUpdate(this);
-                }
-                Thread.sleep(timeInterval);
-            } catch (InterruptedException e) {
-                break;
-            } catch (Exception e) {
-                if (callback != null) callback.onError(e);
-                else e.printStackTrace();
+    public void stopThread(final ApiDataType dataType) {
+        Thread workerThread = threadMap.get(dataType);
+        if(workerThread != null) {
+            if(workerThread.isAlive()) {
+                workerThread.interrupt();
             }
+            threadMap.remove(dataType);
+        }
+    }
+
+    public void startThread(final ApiDataType dataType) {
+        stopThread(dataType);
+        Thread workerThread = new Thread(new ApiWorkerTask(dataType));
+        threadMap.put(dataType, workerThread);
+        workerThread.start();
+    }
+
+    public void stopAllThreads() {
+        for(ApiDataType dataType : threadMap.keySet()) {
+            stopThread(dataType);
         }
     }
 
@@ -63,20 +130,6 @@ public class ApiWorker implements Runnable {
 
     public ApiWorker setPair(Object pair) {
         this.pair = pair;
-        return this;
-    }
-
-    public ApiWorker setAccountInfoUpdate(boolean updateAccountInfo, boolean retrieveOrders, boolean retrieveHistory) {
-        this.updateAccountInfo = updateAccountInfo;
-        this.retrieveAccountOrders = retrieveOrders;
-        this.retrieveAccountHistory = retrieveHistory;
-        return this;
-    }
-
-    public ApiWorker setMarketInfoUpdate(boolean updateMarketInfo, boolean retrieveOrders, boolean retrieveHistory) {
-        this.updateMarketInfo = updateMarketInfo;
-        this.retrieveMarketOrders = retrieveOrders;
-        this.retrieveMarketHistory = retrieveHistory;
         return this;
     }
 
